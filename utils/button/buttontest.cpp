b1de9d7bb1f65952d88197349ee802449672ecb6
unknown <1075518049@qq.com>
2016-06-04 22:56:29 +0800

    empty

--- utils/button/buttontest.cpp
+++ utils/button/buttontest.cpp
@@ -3,7 +3,7 @@
 ButtonTest::ButtonTest(QWidget *parent) :
     QWidget(parent)
 {
-    btn = new BaseButton(MuiFont(), MuiFont::ICON_PENGYOUQUAN, 200, 100);
+    btn = new BaseButton(MuiFont(), MuiFont::ICON_WEINXIN, 200, 100);
 
     lay = new QVBoxLayout(this);
     lay->addWidget(btn);
