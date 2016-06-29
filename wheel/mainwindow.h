b6e135315705b61a6b2be43585490ffe220c93da
浓咖啡 <1075518049@qq.com>
2016-06-29 22:26:35 +0800

    empty

--- wheel/mainwindow.h
+++ wheel/mainwindow.h
@@ -37,13 +37,13 @@ public:
         preDay = today.addDays(-1);
         int curYear = today.year();
         while(curYear == nextDay.year()){
-            dayList.append(nextDay.toString("MM-dd"));
+            dayList.append(nextDay.toString("MM-dd dddd"));
             nextDay = nextDay.addDays(1);
         }
 
         int todayIndex=0;
         while(curYear == preDay.year()){
-            dayList.prepend(preDay.toString("MM-dd"));
+            dayList.prepend(preDay.toString("MM-dd dddd"));
             preDay = preDay.addDays(-1);
             todayIndex++;
         }
@@ -58,19 +58,20 @@ public:
 //        m_wheel1->setFixedSize(100,100);
 
         QStringList hours;
-        hours << "1" << "2" << "3" << "4" << "5" << "6" << "7" << "8" << "9" << "10"<< "11"<< "12";
+        QString str;
+        for(int i=1; i<=24; i++){
+            hours.append(str.number(i));
+        }
+//        hours << "1" << "2" << "3" << "4" << "5" << "6" << "7" << "8" << "9" << "10"<< "11"<< "12";
         m_wheel2 = new StringWheelWidget(touch);
         m_wheel2->setItems( hours );
         grid->addWidget( m_wheel2, 0, 1 );
 
         QStringList minites;
-        minites << "00" <<"01" <<"02" <<"03" <<"04" <<"05" <<"06" <<"07" <<"08" <<\
-                   "09" <<"10" <<"11" <<"12" <<"13" <<"14" <<"15" <<"16" <<"17" <<\
-                   "18" <<"19" <<"20" <<"21" <<"22" <<"23" <<"24" <<"25" <<"26" <<\
-                   "27" <<"28" <<"29" <<"30" <<"31" <<"32" <<"33" <<"34" <<"35" <<\
-                   "36" <<"37" <<"38" <<"39" <<"40" <<"41" <<"42" <<"43" <<"44" <<\
-                   "45" <<"46" <<"47" <<"48" <<"49" <<"50" <<"51" <<"52" <<"53" <<\
-                   "54" <<"55" <<"56" <<"57" <<"58" <<"59";
+        minites << "00" <<"01" <<"02" <<"03" <<"04" <<"05" <<"06" <<"07" <<"08" <<"09";
+        for(int i=10; i<=59; i++){
+            minites.append(str.number(i));
+        }
         m_wheel3 = new StringWheelWidget(touch);
         m_wheel3->setItems( minites );
         grid->addWidget( m_wheel3, 0, 2 );
