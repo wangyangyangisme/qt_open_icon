e559e1be7d76d74b5e9c87db21055a25411d3072
浓咖啡 <1075518049@qq.com>
2016-10-23 17:33:47 +0800

    修改注释

--- service/database/databasetest.h
+++ service/database/databasetest.h
@@ -2,7 +2,7 @@
  ** @file:              abstractdao.h
  ** @author:	    浓咖啡
  ** @date:            2016.10.23
- ** @brief:          数据库测试:这里只是测试了基本框架完整性，对于多线程测试，可以另写测试用例
+ ** @brief:          数据库测试(service层):这里只是测试了基本框架完整性，对于多线程测试，可以另写测试用例
  */
 
 #ifndef DATABASETEST_H
